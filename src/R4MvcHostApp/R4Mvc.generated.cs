c11ac7eca1883947573ab3b85c6078a11a8d84c0
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-02-17 21:21:43 +0000

    Re-ran generator on clean R4Mvc.generated.cs file

--- src/R4MvcHostApp/R4Mvc.generated.cs
+++ src/R4MvcHostApp/R4Mvc.generated.cs
@@ -30,6 +30,178 @@ namespace R4MvcHostApp.Controllers
         [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
         public class ActionNamesClass
         {
+        }
+
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNameConstants
+        {
+        }
+
+        static readonly ViewsClass s_views = new ViewsClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ViewsClass
+        {
+            static readonly _ViewNamesClass s_ViewNames = new _ViewNamesClass();
+            public class _ViewNamesClass
+            {
+            }
+        }
+    }
+
+    [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+    public partial class R4MVC_AccountController : R4MvcHostApp.Controllers.AccountController
+    {
+        public R4MVC_AccountController()
+        {
+        }
+    }
+
+    public partial class R4MVC_AccountController
+    {
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public R4MVC_AccountController()
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Name = "R4MVC_AccountController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public const string NameConst = "R4MVC_AccountController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Area = "R4MVC_AccountController";
+        static readonly ActionNamesClass s_actions = new ActionNamesClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNamesClass
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNameConstants
+        {
+        }
+
+        static readonly ViewsClass s_views = new ViewsClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ViewsClass
+        {
+            static readonly _ViewNamesClass s_ViewNames = new _ViewNamesClass();
+            public class _ViewNamesClass
+            {
+            }
+        }
+    }
+
+    [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+    public partial class R4MVC_R4MVC_AccountController : R4MvcHostApp.Controllers.R4MVC_AccountController
+    {
+        public R4MVC_R4MVC_AccountController()
+        {
+        }
+    }
+
+    public partial class HomeController
+    {
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public HomeController()
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Name = "HomeController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public const string NameConst = "HomeController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Area = "HomeController";
+        static readonly ActionNamesClass s_actions = new ActionNamesClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNamesClass
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNameConstants
+        {
+        }
+
+        static readonly ViewsClass s_views = new ViewsClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ViewsClass
+        {
+            static readonly _ViewNamesClass s_ViewNames = new _ViewNamesClass();
+            public class _ViewNamesClass
+            {
+            }
+        }
+    }
+
+    [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+    public partial class R4MVC_HomeController : R4MvcHostApp.Controllers.HomeController
+    {
+        public R4MVC_HomeController()
+        {
+        }
+    }
+
+    public partial class R4MVC_HomeController
+    {
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public R4MVC_HomeController()
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Name = "R4MVC_HomeController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public const string NameConst = "R4MVC_HomeController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Area = "R4MVC_HomeController";
+        static readonly ActionNamesClass s_actions = new ActionNamesClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNamesClass
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNameConstants
+        {
+        }
+
+        static readonly ViewsClass s_views = new ViewsClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ViewsClass
+        {
+            static readonly _ViewNamesClass s_ViewNames = new _ViewNamesClass();
+            public class _ViewNamesClass
+            {
+            }
+        }
+    }
+
+    [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+    public partial class R4MVC_R4MVC_HomeController : R4MvcHostApp.Controllers.R4MVC_HomeController
+    {
+        public R4MVC_R4MVC_HomeController()
+        {
+        }
+    }
+
+    public partial class AccountController
+    {
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public AccountController()
+        {
+        }
+
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Name = "AccountController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public const string NameConst = "AccountController";
+        [GeneratedCode("R4MVC", "1.0")]
+        public readonly string Area = "AccountController";
+        static readonly ActionNamesClass s_actions = new ActionNamesClass();
+        [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
+        public class ActionNamesClass
+        {
             public readonly string Login = "Login";
             public readonly string Register = "Register";
             public readonly string Manage = "Manage";
@@ -132,6 +304,10 @@ namespace R4MVC
 [GeneratedCode("R4MVC", "1.0"), DebuggerNonUserCode]
 public static partial class MVC
 {
+    public static R4MvcHostApp.Controllers.AccountController Account = new R4MvcHostApp.Controllers.AccountController();
+    public static R4MvcHostApp.Controllers.R4MVC_AccountController R4MVC_Account = new R4MvcHostApp.Controllers.R4MVC_AccountController();
+    public static R4MvcHostApp.Controllers.HomeController Home = new R4MvcHostApp.Controllers.HomeController();
+    public static R4MvcHostApp.Controllers.R4MVC_HomeController R4MVC_Home = new R4MvcHostApp.Controllers.R4MVC_HomeController();
     public static R4MvcHostApp.Controllers.AccountController Account = new R4MvcHostApp.Controllers.AccountController();
     public static R4MvcHostApp.Controllers.HomeController Home = new R4MvcHostApp.Controllers.HomeController();
 }
